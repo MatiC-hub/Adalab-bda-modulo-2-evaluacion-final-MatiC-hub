c8bf66e9d4a280516b42a8077a0076d6f9102168
MatiC-Hub <gpsalegre@gmail.com>
2025-12-02 19:59:10 +0100

    Últimas modificaciones

--- Ejercicio_1_Consultas.sql
+++ Ejercicio_1_Consultas.sql
@@ -3,9 +3,15 @@
  from peliculas
  where duracion > 120;
  
- -- ¿Cuántas películas incluyen subtítulos en español? -- NO ESTÁN INCLUIDOS LOS SUBTÍTULOS EN LA TABLA
+ -- ¿Cuántas películas incluyen subtítulos en español? -- NO ESTABAN INCLUIDOS LOS SUBTÍTULOS EN LA TABLA PRIMERA // ALTER TABLE DESDE PYTHON PRIMERO
+ select count(*) as pelis_subtitulos_es
+ from peliculas
+ where subtitulos_es = 1;
  
- 
+SELECT titulo, año, subtitulos  -- para ver cúales son
+FROM peliculas
+WHERE subtitulos_es = 1;
+  
  -- ¿Cuántas películas tienen contenido adulto?
 select count(*) as Adultos
 from peliculas
@@ -18,15 +24,44 @@ order by año
 limit 1;
 
 -- Muestra el promedio de duración de las películas agrupado por género.
-
+select genero, round(avg(duracion), 2) as promedio -- redondeo de 2 decimales
+from peliculas
+group by genero
+order by promedio;
 
 -- ¿Cuántas películas por año se han registrado en la base de datos? Ordena de mayor a menor.
-
+select año, count(*) as pelis_año
+from peliculas
+where año is not null
+group by año
+order by pelis_año desc, año desc;
 
 -- ¿Cuál es el año con más películas en la base de datos
+select año, count(*) as año_más_peliculas
+from peliculas
+group by año
+having count(*) = (    -- año con más películas extraída del contador
+	select max(contador)  
+    from (
+		select count(*) as contador  -- subconsulta que cuenta las peliculas por año
+        from peliculas
+        group by año
+	) as recuento_por_año
+);
 
+SELECT año, COUNT(*) AS total_peliculas  -- con order by
+FROM peliculas
+GROUP BY año
+ORDER BY total_peliculas DESC
+LIMIT 1;
 
 -- Obtén un listado de todos los géneros y cuántas películas corresponden a cada uno.
-
+select genero, count(*) as cantidad
+from peliculas
+group by genero
+order by cantidad desc;
 
 -- Muestra todas las películas cuyo título contenga la palabra "Godfather" (puedes usar cualquier palabra).
+select titulo
+from peliculas
+where lower(titulo) like '%Godfather%';  -- sin distinción mayúsculas o minúsculas
--- README.md
+++ README.md
@@ -14,3 +14,13 @@ Por otro lado, había ejercicios en los que había más de una opción por lo qu
 Primeramente he planteado mi resultado y luego lo he pasado por ChatGPT para ver opciones y comprenderlo en profundidad.
 
 Muy buen ejercicio para quitarse el miedo a las consultas y al uso de Bases de Datos.
+
+Ejercicio 1.- Creación de una base de datos de películas extraídas de un API
+
+Se comienza importando las distintas librerías necesarias para todo el proceso de extracción de datos, creación de base de datos e inserción de datos en la misma, todo ello realizado desde Python, siguiendo las instrucciones del ejercicio.
+
+Seguidamente se realizan distintas consultas para obtener información a partir de esos datos, desde SQL.
+
+Una de las consultas se refiere a datos que no estaban inicialmente en la tabla por lo que he planteado una modificación de la tabla (ALTER TABLE) desde python, y la inserción de datos correspondientes, modificando el df.
+
+Todo un reto para pararse a pensar cómo solucionarlo. En este caso he ido preguntando a ChatGPT también para no saltarme ningún paso.
